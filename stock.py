a27412418192e13afc9e60ebaef83920cd9f48ae
DandinPower <tomhot246@gmail.com>
2022-08-22 18:18:34 +0800

    test structure

--- stock.py
+++ stock.py
@@ -44,6 +44,7 @@ class OperationStock(tf.keras.Model):
         output1 = self.lstm(inputs)
         output2 = self.dense1(output1)
         output3 = self.dense2(output2)
+        self.logger.AddNewLog([output1, output2], "matmul")
         return output3 
 
 class QuantStock(tf.keras.Model):
